ac31a0b31ec4dbc7ef0e66c2bfb995546c2fdff4
Benjamin Grewell <benjamin.grewell@intel.com>
2024-02-14 11:17:57 -0800

    fix: fix typo

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,13 +7,13 @@
 class platform::install {
 
   platform::utils::manage_file { 'platform::netcheck_file':
-    ensure_state => $::platform::ensure_netcheck,
+    ensure => $::platform::ensure_netcheck,
     source       => 'puppet:///modules/platform/tools/netcheck/netcheck.py',
     path         => '/usr/local/bin/netcheck',
   }
 
   platform::utils::manage_file { 'platform::puppet_exporter_file':
-    ensure_state => $::platform::ensure_puppet_exporter,
+    ensure => $::platform::ensure_puppet_exporter,
     source       => 'puppet:///modules/platform/tools/exporter/puppet-agent-exporter',
     path         => '/usr/local/bin/puppet-agent-exporter',
     notify       => Service['platform::puppet-agent-exporter'],
