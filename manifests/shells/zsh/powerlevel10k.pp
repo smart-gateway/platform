98db1cea992eebaf020be100fd37ac838f3161e8
Benjamin Grewell <benjamin.grewell@intel.com>
2024-02-15 08:25:19 -0800

    feat: add stubs for shell management

---
+++ manifests/shells/zsh/powerlevel10k.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @example
+#   platform::shells::zsh::powerlevel10k { 'namevar': }
+define platform::shells::zsh::powerlevel10k (
+) {
+}
