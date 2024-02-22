7f2c17449d66dbec56316df830210ca7d7300a36
Benjamin Grewell <benjamin.grewell@intel.com>
2024-02-22 15:54:04 -0800

    feat: add basic zsh support

--- manifests/shells/zsh.pp
+++ manifests/shells/zsh.pp
@@ -4,5 +4,45 @@
 #
 # @example
 #   include platform::shells::zsh
-class platform::shells::zsh {
+class platform::shells::zsh (
+  String $managed_startup_scripts_global_dir,
+  String $system_path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin',
+  Boolean $manage_startup_scripts = true,
+) {
+  if $manage_startup_scripts {
+    $global_scripts_dir = sprintf($managed_startup_scripts_global_dir, 'zsh')
+
+    # Ensure global directory exists
+    file { $global_scripts_dir:
+      ensure => directory,
+      purge  => true,
+    }
+
+    # Modify skel files
+    file { '/etc/skel/.zshrc':
+      ensure  => file,
+      content => epp('platform/shells/zsh/etc/skel/.zshrc.epp'),
+    }
+
+    file { '/etc/skel/.zprofile':
+      ensure  => file,
+      content => epp('platform/shells/zsh/etc/skel/.zprofile.epp'),
+    }
+
+    # Modify global configuration files
+    file { '/etc/zshenv':
+      ensure  => file,
+      content => epp('platform/shells/zsh/etc/zshenv.epp', { 'path' => $system_path }),
+    }
+
+    file { '/etc/zprofile':
+      ensure  => file,
+      content => epp('platform/shells/zsh/etc/zprofile.epp'),
+    }
+
+    file { '/etc/zshrc':
+      ensure  => file,
+      content => epp('platform/shells/zsh/etc/zshrc.epp', { 'directory' => $global_scripts_dir }),
+    }
+  }
 }
