16fe6fa6798c0b8030ea260cf2c5f758b1d36639
Benjamin Grewell <benjamin.grewell@intel.com>
2024-02-22 13:20:40 -0800

    remove extra parameter

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,7 +8,6 @@ class platform::config {
   # Setup startup scripts for supported shells
   class { 'platform::shells::bash':
     managed_startup_scripts_global_dir => $platform::managed_shell_startup_global_dir,
-    managed_startup_strings_user_dir   => $platform::managed_shell_startup_user_dir,
   }
 
   include platform::shells::zsh
