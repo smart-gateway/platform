0c8d4820553cb536a068cfaf4fe5551fb87dd96a
Benjamin Grewell <benjamin.grewell@intel.com>
2024-03-28 11:27:03 -0700

    add debug lines

--- manifests/domain/control.pp
+++ manifests/domain/control.pp
@@ -24,27 +24,28 @@ class platform::domain::control (
         dsc_path   => $full_path,
       }
     }
+  }
 
-    # $normalized_users_ou = $users_ou.replace(/^OU=/, ',OU=')
-    # $ou_components = $normalized_users_ou.split(/,OU=/)
-    # $reversed_ou_components = $ou_components.reverse
-    # Notify { "users_ou: ${users_ou}": }
-    # Notify { "groups_ou: ${groups_ou}": }
-    # Notify { "reversed_ou_components: ${reversed_ou_components}": }
-    # $current_path = $domain_dn
-    #
-    # $ou_components.each | $index, $ou | {
-    #   $full_dn = "OU=${ou},${current_path}"
-    #   Notify { "${index} | full_dn: ${full_dn}": }
-    #   Notify { "${index} | current_path: ${current_path}": }
-    #   Notify { "${index} | ou: ${ou}": }
-    #
-    #   dsc_adorganizationalunit { "ensure ${full_dn} is created":
-    #     dsc_ensure => 'present',
-    #     dsc_name   => $ou,
-    #     dsc_path   => $current_path,
-    #   }
-    #
-    #   $current_path = $full_dn
-    # }
+  # $normalized_users_ou = $users_ou.replace(/^OU=/, ',OU=')
+  # $ou_components = $normalized_users_ou.split(/,OU=/)
+  # $reversed_ou_components = $ou_components.reverse
+  # Notify { "users_ou: ${users_ou}": }
+  # Notify { "groups_ou: ${groups_ou}": }
+  # Notify { "reversed_ou_components: ${reversed_ou_components}": }
+  # $current_path = $domain_dn
+  #
+  # $ou_components.each | $index, $ou | {
+  #   $full_dn = "OU=${ou},${current_path}"
+  #   Notify { "${index} | full_dn: ${full_dn}": }
+  #   Notify { "${index} | current_path: ${current_path}": }
+  #   Notify { "${index} | ou: ${ou}": }
+  #
+  #   dsc_adorganizationalunit { "ensure ${full_dn} is created":
+  #     dsc_ensure => 'present',
+  #     dsc_name   => $ou,
+  #     dsc_path   => $current_path,
+  #   }
+  #
+  #   $current_path = $full_dn
+  # }
 }
