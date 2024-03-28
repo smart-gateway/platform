e4ef404ccb4e0175a49ee66da5903804d2fe7dcc
Benjamin Grewell <benjamin.grewell@intel.com>
2024-03-28 10:17:46 -0700

    modify user and group ou defaults

--- manifests/domain/control.pp
+++ manifests/domain/control.pp
@@ -6,15 +6,34 @@
 #   include platform::domain::control
 class platform::domain::control (
   String $domain_dn = 'DC=jointpathfinding,DC=com',
-  String $user_dn = "CN=Users,${domaindn}",
-  String $group_name = 'Groups',
-  String $sudoers_name = 'sudoers',
+  String $users_ou = 'OU=Users,OU=Research',
+  String $groups_ou = 'OU=Groups,OU=Research',
+  String $sudoers_ou = 'OU=sudoers',
 ) {
   # Ensure that this system is a domain controller
-  dsc_adorganizationalunit { 'ensure_groups_ou':
-    dsc_ensure => 'present',
-    dsc_name   => $group_name,
-    dsc_path   => $domain_dn,
+  if $facts['is_domain_controller'] {
+    # Ensure the OUs for the users and groups is created
+    $ou_components = $users_ou.split(/,OU=/).reverse
+    $current_path = $domain_dn
+
+    $ou_components.each | $ou | {
+      $full_dn = "OU=${ou},${current_path}"
+
+      dsc_adorganizationalunit { "ensure ${full_dn} is created":
+        dsc_ensure => 'present',
+        dsc_name   => $ou,
+        dsc_path   => $current_path,
+      }
+
+      $current_path = $full_dn
+    }
+
+    # # Ensure the
+    # dsc_adorganizationalunit { 'ensure_groups_ou':
+    #   dsc_ensure => 'present',
+    #   dsc_name   => $group_name,
+    #   dsc_path   => $domain_dn,
+    # }
   }
 
   #         ADOrganizationalUnit 'ExampleOU'
