2754fe25d5ff0fe10ee4e798cf52f8455334c43a
Benjamin Grewell <benjamin.grewell@intel.com>
2024-04-03 16:39:37 -0700

    fix missing home_dir

--- manifests/user/domain.pp
+++ manifests/user/domain.pp
@@ -11,6 +11,10 @@ define platform::user::domain (
   String $managed_startup_scripts_user_dir,
 ) {
   # Run initialization for each shell so that the user can move back and forth with consistency
+  $home_dir = "/home/${username}"
+  $ssh_directory = "${home_dir}/.ssh"
+  $authorized_keys_path = "${ssh_directory}/authorized_keys"
+
   # Ensure users bash customizations are executed
   platform::shells::bash_user { "bash_user_${username}":
     username                         => $username,
