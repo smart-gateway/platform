3f4d6879ae0377956ca339086fb5ce2129d7dee3
Benjamin Grewell <benjamin.grewell@intel.com>
2024-04-03 16:23:51 -0700

    update home_directories fact

--- lib/facter/home_directories.rb
+++ lib/facter/home_directories.rb
@@ -2,13 +2,17 @@
 
 Facter.add(:home_directories) do
   setcode do
+    Facter.debug('Determining home directories')
     if Facter.value(:os)['family'] == 'windows'
       base_dir = 'C:/Users'
     else
       base_dir = '/home'
     end
-    Dir.entries(base_dir).select { |entry|
+    Facter.debug("Base directory: #{base_dir}")
+    directories = Dir.entries(base_dir).select { |entry|
       File.directory?(File.join(base_dir, entry)) && !(entry == '.' || entry == '..')
     }.map { |entry| "#{base_dir}/#{entry}" }
+    Facter.debug("Directories found: #{directories}")
+    directories
   end
 end
