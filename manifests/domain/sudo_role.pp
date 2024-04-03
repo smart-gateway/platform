ea72741ed00dfcdf30c256e41b3ed75c51b1bc15
Benjamin Grewell <benjamin.grewell@intel.com>
2024-04-03 06:59:37 -0700

    experiment with unless condition

--- manifests/domain/sudo_role.pp
+++ manifests/domain/sudo_role.pp
@@ -17,7 +17,8 @@ define platform::domain::sudo_role (
     exec { "create-${role_name}":
       command   => "New-ADObject -Name '${role_name}' -Path '${path}' -Type sudoRole -OtherAttributes @{sudoCommand='${sudo_command}'; sudoHost='${sudo_host}'; sudoUser='${sudo_user}'}",
       provider  => powershell,
-      unless    => "if (-not (dsquery * -scope base -filter '(&(objectCategory=*)(name=${role_name}))' '${path}')) { Exit 1 } else { Exit 0 }",
+      unless    => "\$obj = Get-ADObject -Filter 'Name -eq ${role_name}' -SearchBase ${path};
+                   if (${obj} -eq \$null) { Exit 1 } else { Exit 0 }",
       logoutput => true,
     }
   } elsif $ensure == 'absent' {
